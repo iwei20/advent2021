bcad68b8a5d3c2f66512a58f16b23e737428d1f8
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2021-12-05 15:00:23 -0500

    Day 5 solution

--- 5/1/main.cpp
+++ 5/1/main.cpp
@@ -5,13 +5,30 @@ using ll = long long;
 using vi = vector<int>;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-
     ifstream fin("../input.in");
     string s;
-    fin >> s;
-    cout << s;
-    cout << smanip::slice("Hello", -2, nullopt, -2) << "\n";
+    
+    bool part1 = true;
+    vector<vector<int>> pipes(1000, vector<int>(1000));
+    int count = 0;
+
+    while(getline(fin, s)) {
+        
+        int a, b, c, d;
+        sscanf(s.c_str(), "%i,%i -> %i,%i\n", &a, &b, &c, &d);
+
+        int dx = (a < c) - (a > c);
+        int dy = (b < d) - (b > d);
+        
+        if (part1 && abs(dx) == abs(dy)) continue;
+
+        for (int i = a, j = b; ; i += dx, j += dy) {
+            ++pipes[i][j];
+            count += (pipes[i][j] == 2);
+            if(i == c && j == d) break;
+        }
+    }
+
+    cout << count << "\n";
+    
 }
