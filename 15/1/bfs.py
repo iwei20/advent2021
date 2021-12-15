c36bb4b341cc97fd068337e97fe413da246022b6
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2021-12-15 01:05:29 -0500

    Separate p1 and p2

--- 15/1/bfs.py
+++ 15/1/bfs.py
@@ -4,26 +4,13 @@ lines = [l.rstrip() for l in open("../input.in", "r+").readlines()]
 
 dr = [-1, 0, 1, 0]
 dc = [0, -1, 0, 1]
-dRC = zip(dr, dc)
 
 graph = [[int(c) for c in line] for line in lines]
-newgraph = [[0 for i in range(5 * len(lines[0]))] for j in range(5 * len(lines))]
 
-for i in range(5):
-    for j in range(5):
-        for r in range(i * len(lines), (i + 1) * len(lines)):
-            
-            for c in range(j * len(lines[0]), (j + 1) * len(lines[0])):
-                
-                newgraph[r][c] = graph[r - i * len(lines)][c - j * len(lines[0])] + i + j
-                if newgraph[r][c] > 9:
-                    newgraph[r][c] -= 9
-
-
-lowest = [[10000000 for ele in row] for row in newgraph]
+lowest = [[10000000 for ele in row] for row in graph]
 
 def in_bounds(r, c):
-    return r >= 0 and r < len(newgraph) and c >= 0 and c < len(newgraph[0])
+    return r >= 0 and r < len(graph) and c >= 0 and c < len(graph[0])
 
 bfs = []
 heappush(bfs, (0, 0, 0))
@@ -32,7 +19,7 @@ lowest[0][0] = 0
 while len(bfs) > 0:
     ele = heappop(bfs)
     
-    if ele[1:] == (len(newgraph) - 1, len(newgraph[0]) - 1):
+    if ele[1:] == (len(graph) - 1, len(graph[0]) - 1):
         continue
 
     for dR, dC in zip(dr, dc):
@@ -40,7 +27,7 @@ while len(bfs) > 0:
         if not in_bounds(nr, nc):
             continue
 
-        nrisk = newgraph[nr][nc] + ele[0]
+        nrisk = graph[nr][nc] + ele[0]
         if lowest[nr][nc] > nrisk:
             
             heappush(bfs, (nrisk, nr, nc))
